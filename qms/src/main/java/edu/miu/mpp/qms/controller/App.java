53f9df7485a92469dc17657d33152fa79da45c45
kashifjawad <kashifjawad@10.10.34.32>
2021-02-28 09:49:40 -0600

    Student & professor Dashboard

--- qms/src/main/java/edu/miu/mpp/qms/controller/App.java
+++ qms/src/main/java/edu/miu/mpp/qms/controller/App.java
@@ -13,20 +13,23 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-    private static Scene scene;
+	 private static Scene scene;
+//	 public static Stage stage;
+
 
     @Override
     public void start(Stage stage) throws IOException {
+//    	App.stage = stage;
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    public static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
+    public static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
