d7e600588bf7dd5e808dd2e9145c944dec4a649b
Yaya <saikoudiallo59@gmail.com>
2021-03-03 21:22:28 -0600

    generic controller

---
+++ qms/src/main/java/edu/miu/mpp/qms/controller/Controller.java
@@ -0,0 +1,15 @@
+package edu.miu.mpp.qms.controller;
+
+public class Controller {
+	
+	private Object data;
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object obj) {
+		this.data = obj;
+	}
+
+}
