c920f7a25b1349239de48f2412aa7c1a4b50dc5a
kashifjawad <kashifjawad@172.19.140.59>
2021-03-04 01:18:56 -0600

    validations on login and register

--- qms/src/main/java/edu/miu/mpp/qms/controller/RegistrationController.java
+++ qms/src/main/java/edu/miu/mpp/qms/controller/RegistrationController.java
@@ -9,6 +9,7 @@ import edu.miu.mpp.qms.business.UserType;
 import edu.miu.mpp.qms.dao.LoadData;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
@@ -32,9 +33,46 @@ public class RegistrationController {
     @FXML
     private Button register;
     
+    String validateMessage ;
+    public void validatingFields() {
+    	validateMessage = "";
+    	
+    	if (fname.getText().length() < 1) {
+    		validateMessage = "Enter Firstname\n";
+    	}
+    	if (lname.getText().length() < 1) {
+    		validateMessage = validateMessage +"Enter Lastname\n";
+
+    	}
+    	if (username.getText().length() < 1) {
+    		validateMessage = validateMessage +"Enter Username\n";
+
+    	}
+    	
+    	if (pwd.getText().length() < 1) {
+    		validateMessage = validateMessage +"Enter Password\n";
+
+    	}
+    
+    	if(!prof.isSelected() && !stu.isSelected()) {
+    		validateMessage = validateMessage +"Select Any Role\n";
+
+    	}
+    	
+    	
+    }
+    
+    
 	@FXML
     void registerationAction(ActionEvent event) throws IOException {
 		//verify is is teacher or student
+		
+		validatingFields();
+    	if (validateMessage.length() > 0) {
+    		alertMessage();
+    		return;
+    	}
+		
 		User newUser = new User(username.getText(), pwd.getText(), fname.getText(), lname.getText());
 		newUser.setUserType(prof.isSelected() ? UserType.PROFESSOR : UserType.STUDENT);
 		LoadData.getUsers().add(newUser);
@@ -50,6 +88,15 @@ public class RegistrationController {
     void logoutAction(ActionEvent event) throws IOException {
     	 App.setRoot("login");
     }
+    
+    public void alertMessage() {
+    	  Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    	    alert.setTitle("Alert");
+    	    alert.setHeaderText("Data Missing");
+    	    alert.setContentText(validateMessage);
+    	    alert.showAndWait();
+   
+}
 
 
 }
